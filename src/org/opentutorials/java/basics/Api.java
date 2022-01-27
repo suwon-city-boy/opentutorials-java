8e5d1647525195b1086025eafb847bd10acf2bd8
suwon-city-boy <kimsungkyu.dev@gmail.com>
2022-01-27 18:23:43 +0900

    feat: API 관련 실습 파일 추가

---
+++ src/org/opentutorials/java/basics/Api.java
@@ -0,0 +1,21 @@
+package org.opentutorials.java.basics;
+
+public class Api {
+
+	public static void main(String[] args) {
+
+	}
+}
+
+/*
+ * API란 자바 시스템을 제어하기 위해서 자바에서 제공하는 명령어
+ * Java SE(JDK)를 설치하면 자바 시스템을 제어하기 위한 API를 제공
+ * 자바 에플리케이션을 만든다는 것은 결과적으로 자바에서 제공하는 패키지들을 부품으로 조립해서 사용자 정의 로직을 만드는 과정
+ * API Documentation을 통해 각종 문서 확인 가능
+ 
+ * 자바에서 제공하는 대표적인 기본 패키지들
+ * java.lang : 자바 프로그래밍을 위한 가장 기본적인 패키지와 클래스를 포함하고 있다.
+ * java.util : 프로그램을 제어하기 위한 클래스와 데이터를 효율적으로 저장하기 위한 클래스들을 담고 있다.
+ * java.io : 키보드, 모니터, 프린터, 파일등을 제어할 수 있는 클래스들의 모음
+ * java.net : 통신을 위한 기능들을 담고 있다.
+ */
